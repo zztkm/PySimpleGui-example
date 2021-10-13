cb8ca535cf2c041b30e81430b3238d08eb4de218
zztkm <takumi.basket1682@gmail.com>
2021-10-13 17:03:08 +0900

    ssl checker を導入

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import json
+
 import PySimpleGUI as sg
+from PySimpleGUI.PySimpleGUI import popup
+
+from ssl_checker import SSLChecker
+
+SSLChecker = SSLChecker()
 
 # デザインテーマの設定
 sg.theme("DarkAmber")
 
 # windowに配置するコンポーネントの設定
 layout = [
-    [sg.Text("1行目")],
-    [sg.Text("2行目: あつまれ俺の森!!!"), sg.InputText()],
-    [sg.Button("OK"), sg.Button("Cancel")]
+    [sg.Text("SSL対応かチェックしたいドメインを入力してくれ"), sg.InputText()],
+    [sg.Button("チェック実行"), sg.Button("Cancel")]
 ]
 
 
@@ -20,8 +26,17 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED or event == "Cancel":
         break
-    elif event == "OK":
-        print("Your Input: ", values[0])
+    elif event == "チェック実行":
+        host = values[0]
+        args = {
+            "hosts": [host]
+        }
+        cert = SSLChecker.get_cert(host, 443, SSLChecker.get_args(json_args=args))
+        result = SSLChecker.get_cert_info(host, cert)
+        print(result["host"])
+        print(result["cert_valid"])
+        popup_msg = "SSL対応している！" if result["cert_valid"] else "SSL対応していません。しっかりしてください！"
+        sg.popup(f"Your Input: {popup_msg}")
 
 
 window.Close()
